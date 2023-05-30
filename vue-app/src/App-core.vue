72bde7fc829053a33a5db9cc85fe218f575c558a
Subramanian Murugan <sasubramanian_md@hotmail.com>
2023-05-30 15:48:49 +0530

    first commit

---
+++ vue-app/src/App-core.vue
@@ -0,0 +1,11 @@
+<!-- SFC Pattern -->
+<script>
+// Component state and logic
+</script>
+<template>
+  <!-- Component Markup -->
+  <h1>Welcome to Vue Application</h1>
+</template>
+<style>
+/* Component Specific Styles */
+</style>
---
+++ vue-app/src/App.vue
@@ -0,0 +1,11 @@
+<!-- Options Api Pattern -->
+<script>
+export  default{
+
+}
+</script>
+<template>
+ <h1>Vue SFC Component Using Options Api</h1>
+</template>
+<style>
+</style>
